0003a46eb2e63bce9873a1339c673548203aeb87
TerangAhn <akh960424@gmail.com>
2024-10-30 14:49:56 +0900

    태랑1030

--- src/main/java/com/postgre/choongsam/controller/AtrController.java
+++ src/main/java/com/postgre/choongsam/controller/AtrController.java
@@ -1,8 +1,16 @@
 package com.postgre.choongsam.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AtrController {
+	
+	@RequestMapping(value = "/registerCourseForm")
+	public String registerCourseForm(){
+		
+		return "view_Atr/registerCourseForm";
+	}
+	
 
 }
