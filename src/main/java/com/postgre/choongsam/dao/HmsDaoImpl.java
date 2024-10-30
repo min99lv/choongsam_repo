96ce4de3a30f60accefaffe15fe6bdff55ddeea0
hms0725 <hms910725@naver.com>
2024-10-30 14:40:50 +0900

    ms 1030-1

---
+++ src/main/java/com/postgre/choongsam/dao/HmsDaoImpl.java
@@ -0,0 +1,11 @@
+package com.postgre.choongsam.dao;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class HmsDaoImpl implements HmsDao {
+
+}
