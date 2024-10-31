a8f908126f2c0caa21c872f42c27525ddb9495e1
dinghou <sickhoo1@naver.com>
2024-10-31 16:07:45 +0900

    jh commit 1031

--- src/main/java/com/postgre/choongsam/service/HjhService.java
+++ src/main/java/com/postgre/choongsam/service/HjhService.java
@@ -1,5 +1,11 @@
 package com.postgre.choongsam.service;
 
+import java.util.List;
+
+import com.postgre.choongsam.dto.User_Info;
+
 public interface HjhService {
+
+	List<User_Info> userList();
 
 }
