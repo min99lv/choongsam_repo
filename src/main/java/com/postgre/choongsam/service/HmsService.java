839d0ab96cf2fe580a6155719c946e3cb50679af
hms0725 <hms910725@naver.com>
2024-10-31 15:59:58 +0900

    ms_mapper,service,controller,view

--- src/main/java/com/postgre/choongsam/service/HmsService.java
+++ src/main/java/com/postgre/choongsam/service/HmsService.java
@@ -1,7 +1,13 @@
 package com.postgre.choongsam.service;
 
+import java.util.List;
+
+import com.postgre.choongsam.dto.Lecture_Video;
+
 public interface HmsService {
 
-	String getVideoDetails(String videoId);
+	String getVideoDetails(String videoId); //영상화면
+
+	List<Lecture_Video> findAllVideo(); //모든 비디오 목록
 
 }
