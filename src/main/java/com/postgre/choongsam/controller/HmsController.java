f3783b002e7897e55507e2a6394af7b7cf774d62
hms0725 <hms910725@naver.com>
2024-10-31 15:59:58 +0900

    ms_mapper,service,controller,view

--- src/main/java/com/postgre/choongsam/controller/HmsController.java
+++ src/main/java/com/postgre/choongsam/controller/HmsController.java
@@ -1,5 +1,7 @@
 package com.postgre.choongsam.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,21 +23,32 @@ public class HmsController {
 	private final HmsService HS;
 	
 	
-	
+	//영상불러오기 api
 	@GetMapping("/video-details")
 	public String getVideoDetails(@RequestParam String videoId, Model model) {
-		log.info("Received videoId: {}", videoId);
+		System.out.println("hmsController youtube api start..");
+		System.out.println("Received videoId:"+ videoId);
+		
 		//Lecture_video 객체 생성
 		Lecture_Video lectureVideo =  new Lecture_Video();
 		lectureVideo.setConts_id(videoId);
 		
-		
 		//유튜브 url 생성
-		String videoUrl = "http://www.youtube.com/embed/" + videoId;	
+		String videoUrl = "http://www.youtube.com/embed/" + videoId;
+		System.out.println("hmsController videoUrl:"+videoUrl);
 		model.addAttribute("videoUrl", videoUrl);		
 		HS.getVideoDetails(videoId);
 		return "view_Hms/videoView";
 	}
+
+	//postgre 모든 영상 불러오기
+	@GetMapping("/video-list")
+	public String getVideoList(Model model) {
+		List<Lecture_Video> videoList = HS.findAllVideo();
+		model.addAttribute("videoList",videoList);
+		return "view_Hms/videoList";
+	}
+	
 	
 	
 
