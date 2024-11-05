b22f3029bab3d54aea087d85f98fa9eff374de67
min99lv <fantamasitta@gmail.com>
2024-11-05 14:06:20 +0900

    jungmin 1105

--- src/main/java/com/postgre/choongsam/controller/SjmController.java
+++ src/main/java/com/postgre/choongsam/controller/SjmController.java
@@ -4,20 +4,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.postgre.choongsam.dto.Lecture;
 import com.postgre.choongsam.dto.Login_Info;
 import com.postgre.choongsam.dto.Note;
 import com.postgre.choongsam.dto.Notice;
 import com.postgre.choongsam.dto.Paging;
 import com.postgre.choongsam.service.SjmService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,103 +31,201 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class SjmController {
 	private final SjmService ss;
-	
-	// NOTE -  공지사항 목록
-	@GetMapping(value="/api/notice")
-	public String notice(Model model,
-			@RequestParam(value = "currentPage", defaultValue = "1") String currentPage,
-			@RequestParam(value = "keyword", required = false) String keyword
-			) 
-			{
-		
-		
-				log.info("noticeController start...");
-				int total = ss.countNotice(keyword); 
-				Paging page = new Paging(total,currentPage);
-				// Map 객체 생성하여 파라미터 설정
-				Map<String, Object> params = new HashMap<>();
-				params.put("start", page.getStart());
-				params.put("rowPage", page.getRowPage());
-				params.put("keyword", keyword);
-				
-				System.out.println("page---->"+page);
-				
-				List<Notice> noticeList = ss.selectNoticeList(params);
-				
-				
-				
-				model.addAttribute("page",page);
-				model.addAttribute("total",total);
-				model.addAttribute("noticeList",noticeList);
-		
-		return "view_Sjm/notice";
-		
+
+	// 공지사항 ------------------------------------------------------------------
+
+	// NOTE - 공지사항 목록
+	@GetMapping(value = "/api/notice")
+	public String notice(Model model, @RequestParam(value = "currentPage", defaultValue = "1") String currentPage,
+			@RequestParam(value = "keyword", required = false) String keyword) {
+
+		log.info("noticeController start...");
+		int total = ss.countNotice(keyword);
+		Paging page = new Paging(total, currentPage);
+		// Map 객체 생성하여 파라미터 설정
+		Map<String, Object> params = new HashMap<>();
+		params.put("start", page.getStart());
+		params.put("rowPage", page.getRowPage());
+		params.put("keyword", keyword);
+
+		System.out.println("page---->" + page);
+
+		List<Notice> noticeList = ss.selectNoticeList(params);
+
+		model.addAttribute("page", page);
+		model.addAttribute("total", total);
+		model.addAttribute("noticeList", noticeList);
+
+		return "view_Sjm/noticeList";
+
 	}
 
-	// NOTE -  공지사항 작성 화면 
-	@GetMapping(value="/api/notice/new")
-	public String noticeCreateForm(){
-		
+	// NOTE - 공지사항 작성화면
+	@GetMapping(value = "/api/notice/new")
+	public String noticeCreateForm() {
+
 		System.out.println("SjmController.noticeCreate start .....");
 
 		return "view_Sjm/noticeCreate";
 	}
 
+	// NOTE - 공지사항 작성
+	@PostMapping(value = "/api/notice/new")
+	public ResponseEntity<Integer> noticeCreate(Notice notice, @RequestParam("files") MultipartFile[] files,
+			HttpServletRequest request) {
+		System.out.println("작성 시작");
+		System.out.println("받은 파일 수: " + files.length); // 파일 수 출력
+
+		// TODO : 1. 1102 공지사항 insert 2. 파일 넣기
+		System.out.println("notice-->" + notice);
+		int result = ss.noticeCreate(notice, files, request);
+
+		return ResponseEntity.ok(result);
+	}
+
+	// NOTE - 공지사항 상세 화면
+	@GetMapping(value = "/view_Sjm/noticeDetail")
+	public String noticeDetailForm(@RequestParam("ntc_mttr_sn") int ntc_mttr_sn, Model model) {
+		model.addAttribute("ntc_mttr_sn", ntc_mttr_sn);
+		return "view_Sjm/noticeDetail"; // view_Sjm/noticeDetail.jsp로 이동
+	}
+
 	// NOTE - 공지사항 상세
-	@GetMapping(value="/api/notice/{ntc_mttr_sn}")
+	@GetMapping(value = "/api/notice/{ntc_mttr_sn}")
 	@ResponseBody
 	public Notice noticeDetail(@PathVariable("ntc_mttr_sn") int ntc_mttr_sn) {
 
 		System.out.println("SjmController.noticeDetailForm() start...");
-		
+
 		Notice notice = ss.noticeDetail(ntc_mttr_sn);
 
 		return notice;
 	}
-	
-	// NOTE - 공지사항 상세 화면 
-	@GetMapping(value="/view_Sjm/noticeDetail")
-	public String noticeDetailForm(@RequestParam("ntc_mttr_sn") int ntc_mttr_sn, Model model) {
-	    model.addAttribute("ntc_mttr_sn", ntc_mttr_sn);  
-	    return "view_Sjm/noticeDetail";  // view_Sjm/noticeDetail.jsp로 이동
-	}
-	
-	
-	// NOTE - 쪽지함 전체쪽지 화면
-	@GetMapping(value = "/view_Sjm/noteBox")
+
+	// 쪽지 ------------------------------------------------------------------
+
+	// 쪽지 목록 조회 화면
+	@GetMapping(value = "/notes")
 	public String noteBoxForm(HttpSession session) {
-		
 		System.out.println("쪽지함 화면");
 
-		System.out.println(session.getAttribute("user"));
-		
-		if(session.getAttribute("user") == null) {
+		System.out.println("쪽지함 화면");
+
+		// 세션에서 user_seq 가져오기 (null 여부 확인)
+		Integer user_seq = (Integer) session.getAttribute("user_seq");
+
+		if (user_seq == null || user_seq == 0) {
 			System.out.println("로그인 하시오");
-			
 			return "redirect:/view_Ljm/loginForm";
 		}
-	
-		
-		return "view_Sjm/noteBox";
+
+		System.out.println(user_seq);
+		return "view_Sjm/noteList";
 	}
-	
-	// NOTE - 쪽지 목록
-	@GetMapping(value="/api/note")
+
+	// NOTE - 안읽은 쪽지 목록
+	@GetMapping(value = "/api/notes/unread")
 	@ResponseBody
-	public List<Note> noteList(HttpSession session) {
-		
-		Login_Info user = (Login_Info) session.getAttribute("user");
-		
-		System.out.println("user.getUser_seq() "+user.getUser_seq() );
-		
+	public List<Note> rcvrnoteList(HttpSession session) {
+
+		 System.out.println("text is of type: " + session.getAttribute("user_seq").getClass().getSimpleName());
 		Map<String, Object> params = new HashMap<>();
-		params.put("user_seq", user.getUser_seq() );
-		
+
+		params.put("user_id", session.getAttribute("user"));
+		params.put("user_status", session.getAttribute("usertype"));
+
+		int user_seq = ss.getUserSeq(params);
+
+		System.out.println("user.getUser_seq() " + user_seq);
+
+		params.put("user_seq", user_seq);
+
 		List<Note> noteList = ss.noteList(params);
-		
-		
+
 		return noteList;
 	}
 
+	// NOTE - 보낸 쪽지 목록
+	@GetMapping(value = "/api/notes/sent")
+	@ResponseBody
+	public List<Note> noteList(HttpSession session) {
 
+		Map<String, Object> params = new HashMap<>();
+
+		params.put("user_id", session.getAttribute("user"));
+		params.put("user_status", session.getAttribute("usertype"));
+
+		int user_seq = ss.getUserSeq(params);
+
+		System.out.println("user.getUser_seq() " + user_seq);
+
+		params.put("user_seq", user_seq);
+
+		List<Note> noteList = ss.getSentNotes(params);
+
+		return noteList;
+	}
+
+	// 쪽지 상세 화면
+	@GetMapping(value = "/note/{note_sn}")
+	public String noteDetailForm(@PathVariable("note_sn") int note_sn, Model model) {
+		System.out.println("쪽지 디테일 화면");
+		model.addAttribute("note_sn", note_sn);
+		return "view_Sjm/noteDetail"; // view_Sjm/noticeDetail.jsp로 이동
+	}
+
+	// NOTE - 쪽지 상세정보 조회
+	@GetMapping(value = "/api/notes/{note_sn}")
+	@ResponseBody
+	public Note getNote(@PathVariable("note_sn") int note_sn) {
+
+		System.out.println("상세 쪽지 시작");
+
+		Note note = ss.getNote(note_sn);
+		return note;
+	}
+
+	//  쪽지 전송 화면
+	@GetMapping(value = "/notes/new")
+	public String CreateNoteForm() {
+		System.out.println("쪽지전송화면");
+
+		return "view_Sjm/noteCreate";
+	}
+
+	// NOTE - 쪽지 전송
+	@PostMapping(value = "/api/notes")
+	@ResponseBody
+	public int createNote(Note note, HttpSession session) {
+
+		System.out.println("Note" + note);
+
+		// note.setRcvr_seq( (int) session.getAttribute("user_seq"));
+
+		int result = ss.createNote(note);
+
+		return result;
+
+	}
+	
+	// NOTE - 내가 듣는 강의 목록
+	@GetMapping(value="/api/lectures/my")
+	@ResponseBody
+	public ResponseEntity<List<Lecture>> getMyLectures(@RequestParam("user_seq") Integer user_seq) {
+       System.out.println("내가 듣는 강의 목록 불러오기 ");
+	
+		List<Lecture> lectures = ss.getMyLectures(user_seq);
+        return ResponseEntity.ok(lectures);
+    }
+	
+	// NOTE - 같은 강의를 듣는 사람들 + 강사 목록
+	@GetMapping(value="/api/lectures/{lectureId}/recipients") // 경로 변수로 lectureId를 받습니다.
+	@ResponseBody
+	public ResponseEntity<List<Lecture>> getMyLectures(@PathVariable("lectureId") int lectureId) {
+	    System.out.println("내가 듣는 강의 목록 불러오기 for lectureId: " + lectureId);
+	    
+	    List<Lecture> lectures = ss.getSameLeceture(lectureId);
+	    return ResponseEntity.ok(lectures);
+	}
+	
 }
---
+++ src/main/webapp/WEB-INF/views/view_Sjm/noteDetail.jsp
@@ -0,0 +1,178 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>공지사항 상세</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            position: relative;
+            background-color: #fdfdfd;
+            top: 120px;
+            width: 1320px;
+            margin: 0 auto;
+            box-sizing: border-box;
+            height: auto;
+            padding: 20px; /* 여백 추가 */
+            font-size: 20px;
+        }
+
+        .list {
+            margin: 50px 0;
+        }
+
+        table {
+            padding: 0;
+            width: 1280px;
+            border-top: 2px solid black;
+        }
+
+        th {
+            width: 200px;
+            color: gray;
+            font-weight: bold;
+            text-align: center;
+            padding: 20px 0;
+            border-bottom: 0.4px solid #e2e8ee;
+            font-size: 20px;
+        }
+
+        td {
+            border-bottom: 0.4px solid #e2e8ee;
+            border-left: 0.4px solid #e2e8ee;
+            cursor: pointer;
+            font-weight: bold;
+            color: #323232;
+            font-size: 14px;
+            height: 50px;
+             padding: 20px;
+              font-size: 20px;
+        }
+        
+		.contentsss{
+		    vertical-align: top; /* 내용 위쪽 정렬 */
+		    height: 500px;
+		    padding: 20px;
+		}
+
+        .manager_pagination {
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 40px;
+        }
+
+        input {
+            width: 950px;
+            height: 50px;
+            font-size: 20px;
+            border: #949494 1px solid;
+            border-radius: none;
+        }
+
+         /* 기존 스타일 및 추가 스타일 */
+         .file-preview {
+            margin-top: 10px;
+            max-width: 100%;
+            height: auto;
+            border: 1px solid #ddd;
+            padding: 10px;
+        }
+
+        textarea {
+            width: 950px;
+            height: 400px;
+            font-size: 20px;
+            border: #949494 1px solid;
+            border-radius: none;
+
+        }
+        
+         button {
+            width: 200px;
+            text-align: center;
+            /* 버튼 가운데 정렬을 위한 추가 스타일 */
+            margin: 20px auto; /* 버튼을 가운데 정렬 */
+            display: block; /* 블록으로 설정 */
+            height: 50px;
+            background-color: #00664F;
+            border: none;
+            color: white;
+        }
+    </style>
+    <script type="text/javascript">
+
+    async function fetchNoteDetail() {
+        try {
+            const response = await fetch(`/api/note/${note_sn}`);
+            if (!response.ok) {
+                throw new Error(`Error fetching notice: ${response.status}`);
+            }
+            const note = await response.json();
+            
+            console.log(note); // note 객체를 콘솔에 출력하여 구조 확인
+
+            // 가져온 데이터를 HTML 요소에 넣기
+            document.getElementById('note_ttl').textContent = note.note_ttl;
+            document.getElementById('note_cn').textContent = note.note_cn;
+
+            // 보낸사람과 받은사람 설정
+            const userSeq = "${sessionScope.user_seq}"; // JSP에서 세션의 user_seq를 가져옴
+            console.log('userSeq:', userSeq); // userSeq 출력
+            console.log('note.sndpty_seq:', note.sndpty_seq); // sndpty_seq 출력
+
+            const senderReceiverRow = document.getElementById('sender_receiver');
+
+            if (userSeq === note.sndpty_seq) {
+                senderReceiverRow.innerHTML = `<th>보낸사람</th><td>` + note.sender_name + `</td>`;
+            } else {
+                senderReceiverRow.innerHTML = `<th>받은사람</th><td>` + note.receiver_name + `</td>`;
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            document.getElementById('note_ttl').textContent = "쪽지를 불러올 수 없습니다.";
+            document.getElementById('note_cn').textContent = error.message;
+        }
+    }
+
+    window.onload = fetchNoteDetail;
+      </script>
+</head>
+<body>
+    <header>
+        <%@ include file="../header.jsp" %>
+    </header>
+
+    
+    <div class="container">
+        <div class="contents">
+            <h1>공지사항 작성</h1>
+        </div>
+            <table class="list">
+                <tr>
+                    <th>제목</th>
+                    <td id="note_ttl"></td>
+                </tr>
+               <tr id="sender_receiver">
+                
+            </tr>
+                <tr>
+                    <th>내용</th>
+                    <td class="contentsss" id="note_cn" ></td>
+                </tr>
+            </table>
+            
+            <button onclick="history.back();">목록</button>
+    </div>
+    
+</body>
+</html>
