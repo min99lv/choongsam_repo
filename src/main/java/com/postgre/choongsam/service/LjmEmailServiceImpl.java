63aa595c7469559028a1061dca43f2ba492d8479
jxxxmxxn <jxxxmxxn@gmail.com>
2024-11-06 16:07:42 +0900

    지민 1106

---
+++ src/main/java/com/postgre/choongsam/service/LjmEmailServiceImpl.java
@@ -0,0 +1,12 @@
+package com.postgre.choongsam.service;
+
+import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class LjmEmailServiceImpl implements LjmEmailService {
+
+}
